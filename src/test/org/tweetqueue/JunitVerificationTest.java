1d5e2fc13e59ec08cb8bd590b1299bfaa91aeefb
zipwow <zipwow@gmail.com>
2013-11-26 14:37:34 -0800

    Initial checkin

---
+++ src/test/org/tweetqueue/JunitVerificationTest.java
@@ -0,0 +1,15 @@
+package org.tweetqueue;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class JunitVerificationTest {
+	
+	@Test
+	public void testJunitSetup() {
+		assertTrue(true);
+	}
+
+}
